315d8ec8123136746e59c5b15ad64d9d24d58c01
katherine <katherinejc103@gmail.com>
2022-09-27 08:06:26 -0400

    commit tree 6af130c7d2dc3d6c077a1717d2aab87550c3546b

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+    attr_accessor :discount, :total
+    
 
+    def initialize(discount= 0)
+        @discount = discount
+        @total = 0
+    end
+   
+
+    def add_item(title, price, quantity= 0)
+        new_total = (title, price) + total
+    end
+
+    
+end
